f694c7387068aef9a753fcee92b5a785909cf244
sjpbailey <sjpbailey1961@gmail.com>
2021-08-14 01:31:57 -0700

    CLOSE Back to old way for the weekend

---
+++ nodes/AmiNemController--working-ON.py
@@ -0,0 +1,364 @@
+
+
+"""
+Get the polyinterface objects we need. 
+a different Python module which doesn't have the new LOG_HANDLER functionality
+"""
+import udi_interface
+
+# My Template Node
+from nodes import AmiNemNode
+
+"""
+Some shortcuts for udi interface components
+
+- LOGGER: to create log entries
+- Custom: to access the custom data class
+- ISY:    to communicate directly with the ISY (not commonly used)
+"""
+LOGGER = udi_interface.LOGGER
+LOG_HANDLER = udi_interface.LOG_HANDLER
+Custom = udi_interface.Custom
+ISY = udi_interface.ISY
+
+# IF you want a different log format than the current default
+LOG_HANDLER.set_log_format('%(asctime)s %(threadName)-10s %(name)-18s %(levelname)-8s %(module)s:%(funcName)s: %(message)s')
+
+class AmiNemController(udi_interface.Node):
+    """
+    The Node class represents a node on the ISY. The first node started and
+    that is is used for interaction with the node server is typically called
+    a 'Controller' node. If this node has the address 'controller', Polyglot
+    will automatically populate the 'ST' driver of this node with the node
+    server's on-line/off-line status.
+
+    This node will also typically handle discovery & creation of other nodes
+    and deal with the user configurable options of the node server.
+
+    Class Variables:
+      self.name: String name of the node
+      self.address: String Address of Node, must be less than 14 characters
+                    (ISY limitation)
+      self.primary: String Address of Node's parent, must be less than 14
+                    characters (ISY limitation)
+      self.poly: Interface class object.  Provides access to the interface API.
+
+    Class Methods
+      query(): Queries and reports ALL drivers for ALL nodes to the ISY.
+      getDriver('ST'): gets the current value from Polyglot for driver 'ST'
+        returns a STRING, cast as needed
+      setDriver('ST', value, report, force, uom): Updates the driver with
+        the value (and possibly a new UOM)
+      reportDriver('ST', force): Send the driver value to the ISY, normally
+        it will only send if the value has changed, force will always send
+      reportDrivers(): Send all driver values to the ISY
+      status()
+    """
+    def __init__(self, polyglot, primary, address, name):
+        """
+        Optional.
+        Super runs all the parent class necessities. You do NOT have
+        to override the __init__ method, but if you do, you MUST call super.
+
+        In most cases, you will want to do this for the controller node.
+        """
+        super(AmiNemController, self).__init__(polyglot, primary, address, name)
+        self.poly = polyglot
+        self.name = 'Net Energy Meter Controller'  # override what was passed in
+        self.hb = 0
+
+        
+
+        # Create data storage classes to hold specific data that we need
+        # to interact with.  
+        self.Parameters = Custom(polyglot, 'customparams')
+        self.Notices = Custom(polyglot, 'notices')
+        self.TypedParameters = Custom(polyglot, 'customtypedparams')
+        self.TypedData = Custom(polyglot, 'customtypeddata')
+
+        # Subscribe to various events from the Interface class.  This is
+        # how you will get information from Polyglog.  See the API
+        # documentation for the full list of events you can subscribe to.
+        #
+        # The START event is unique in that you can subscribe to 
+        # the start event for each node you define.
+
+        self.poly.subscribe(self.poly.START, self.start, address)
+        self.poly.subscribe(self.poly.LOGLEVEL, self.handleLevelChange)
+        self.poly.subscribe(self.poly.CUSTOMPARAMS, self.parameterHandler)
+        self.poly.subscribe(self.poly.CUSTOMTYPEDPARAMS, self.typedParameterHandler)
+        self.poly.subscribe(self.poly.CUSTOMTYPEDDATA, self.typedDataHandler)
+        self.poly.subscribe(self.poly.POLL, self.poll)
+
+        # Tell the interface we have subscribed to all the events we need.
+        # Once we call ready(), the interface will start publishing data.
+        self.poly.ready()
+
+        # Tell the interface we exist.  
+        self.poly.addNode(self)
+
+
+
+    def start(self):
+        
+        """
+        The Polyglot v3 Interface will publish an event to let you know you
+        can start your integration. (see the START event subscribed to above)
+
+        This is where you do your initialization / device setup.
+        Polyglot v3 Interface startup done.
+
+        Here is where you start your integration. I.E. if you need to 
+        initiate communication with a device, do so here.
+        """
+
+        # Send the profile files to the ISY if neccessary. The profile version
+        # number will be checked and compared. If it has changed since the last
+        # start, the new files will be sent.
+        self.poly.updateProfile()
+
+        # Send the default custom parameters documentation file to Polyglot
+        # for display in the dashboard.
+        self.poly.setCustomParamsDoc()
+
+        # Initializing a heartbeat is an example of something you'd want
+        # to do during start.  Note that it is not required to have a
+        # heartbeat in your node server
+        #self.heartbeat(0)
+
+        # Device discovery. Here you may query for your device(s) and 
+        # their capabilities.  Also where you can create nodes that
+        # represent the found device(s)
+        self.discover()
+
+        # Here you may want to send updated values to the ISY rather
+        # than wait for a poll interval.  The user will get more 
+        # immediate feedback that the node server is running
+
+    class isy:
+        def __init__(self, isy, poly):
+            # Attributes
+            self.isy = udi_interface.ISY(poly)
+            self.poly = poly
+
+    """
+    Called via the CUSTOMPARAMS event. When the user enters or
+    updates Custom Parameters via the dashboard. The full list of
+    parameters will be sent to your node server via this event.
+
+    Here we're loading them into our local storage so that we may
+    use them as needed.
+
+    New or changed parameters are marked so that you may trigger
+    other actions when the user changes or adds a parameter.
+
+    NOTE: Be carefull to not change parameters here. Changing
+    parameters will result in a new event, causing an infinite loop.
+    """
+    def parameterHandler(self, params):
+        self.Parameters.load(params)
+        LOGGER.debug('Loading parameters now')
+        self.check_params()
+
+    """
+    Called via the CUSTOMTYPEDPARAMS event. This event is sent When
+    the Custom Typed Parameters are created.  See the check_params()
+    below.  Generally, this event can be ignored.
+
+    Here we're re-load the parameters into our local storage.
+    The local storage should be considered read-only while processing
+    them here as changing them will cause the event to be sent again,
+    creating an infinite loop.
+    """
+    def typedParameterHandler(self, params):
+        self.TypedParameters.load(params)
+        LOGGER.debug('Loading typed parameters now')
+        LOGGER.debug(params)
+
+    """
+    Called via the CUSTOMTYPEDDATA event. This event is sent when
+    the user enters or updates Custom Typed Parameters via the dashboard.
+    'params' will be the full list of parameters entered by the user.
+
+    Here we're loading them into our local storage so that we may
+    use them as needed.  The local storage should be considered 
+    read-only while processing them here as changing them will
+    cause the event to be sent again, creating an infinite loop.
+    """
+    def typedDataHandler(self, params):
+        self.TypedData.load(params)
+        LOGGER.debug('Loading typed data now')
+        LOGGER.debug(params)
+
+    """
+    Called via the LOGLEVEL event.
+    """
+    def handleLevelChange(self, level):
+        LOGGER.info('New log level: {}'.format(level))
+
+    """
+    Called via the POLL event.  The POLL event is triggerd at
+    the intervals specified in the node server configuration. There
+    are two separate poll events, a long poll and a short poll. Which
+    one is indicated by the flag.  flag will hold the poll type either
+    'longPoll' or 'shortPoll'.
+
+    Use this if you want your node server to do something at fixed
+    intervals.
+    """
+    def poll(self, flag):
+        if 'longPoll' in flag:
+            LOGGER.debug('longPoll (controller)')
+        else:
+            LOGGER.debug('shortPoll (controller)')
+
+    def query(self,command=None):
+        nodes = self.poly.getNodes()
+        for node in nodes:
+            nodes[node].reportDrivers()
+
+    def discover(self, *args, **kwargs):
+        self.poly.addNode(AmiNemNode(self.poly, self.address, 'aminemnodeid', 'AmiNemNode', self.poly, self.isy, self.nem_oncor))
+
+    def delete(self):
+        LOGGER.info('Net Energy Meter deleted.')
+
+    def stop(self):
+        LOGGER.debug('NodeServer stopped.')
+
+    def set_module_logs(self,level):
+        logging.getLogger('urllib3').setLevel(level)
+
+    def check_params(self):
+        self.Notices.clear()
+        #self.Notices['hello'] = 'Polisy IP is {}'.format(self.poly.network_interface['addr'])
+        default_nem_oncor = ""
+
+        self.nem_oncor = self.Parameters.nem_oncor
+        if self.nem_oncor is None:
+            self.nem_oncor = default_nem_oncor
+            LOGGER.error('check_params: Devisor for Oncor Meters not defined in customParams, please add it.  Using {}'.format(default_nem_oncor))
+            self.nem_oncor = default_nem_oncor    
+
+        # Add a notice if they need to change the user/password from the default.
+        if self.nem_oncor == default_nem_oncor:
+            self.Notices['auth'] = 'Please set proper divisor in configuration page for Landis+Gy set to 1000 set to 10000 for Oncor'
+
+        # Typed Parameters allow for more complex parameter entries.
+        # It may be better to do this during __init__() 
+
+        # Lets try a simpler thing here
+        self.TypedParameters.load( [
+                {
+                    'name': 'template_test',
+                    'title': 'Test parameters',
+                    'desc': 'Test parameters for template',
+                    'isList': False,
+                    'params': [
+                        {
+                            'name': 'id',
+                            'title': 'The Item ID number',
+                            'isRequired': True,
+                        },
+                        {
+                            'name': 'level',
+                            'title': 'Level Parameter',
+                            'defaultValue': '100',
+                            'isRequired': True,
+                        }
+                    ]
+                }
+            ],
+            True
+        )
+
+        '''
+        self.TypedParameters.load( [
+                {
+                    'name': 'item',
+                    'title': 'Item',
+                    'desc': 'Description of Item',
+                    'isList': False,
+                    'params': [
+                        {
+                            'name': 'id',
+                            'title': 'The Item ID',
+                            'isRequired': True,
+                        },
+                        {
+                            'name': 'title',
+                            'title': 'The Item Title',
+                            'defaultValue': 'The Default Title',
+                            'isRequired': True,
+                        },
+                        {
+                            'name': 'extra',
+                            'title': 'The Item Extra Info',
+                            'isRequired': False,
+                        }
+                    ]
+                },
+                {
+                    'name': 'itemlist',
+                    'title': 'Item List',
+                    'desc': 'Description of Item List',
+                    'isList': True,
+                    'params': [
+                        {
+                            'name': 'id',
+                            'title': 'The Item ID',
+                            'isRequired': True,
+                        },
+                        {
+                            'name': 'title',
+                            'title': 'The Item Title',
+                            'defaultValue': 'The Default Title',
+                            'isRequired': True,
+                        },
+                        {
+                            'name': 'names',
+                            'title': 'The Item Names',
+                            'isRequired': False,
+                            'isList': True,
+                            'defaultValue': ['somename']
+                        },
+                        {
+                            'name': 'extra',
+                            'title': 'The Item Extra Info',
+                            'isRequired': False,
+                            'isList': True,
+                        }
+                    ]
+                },
+            ], True)
+            '''
+
+    
+    def remove_notices_all(self,command):
+        LOGGER.info('remove_notices_all: notices={}'.format(self.Notices))
+        # Remove all existing notices
+        self.Notices.clear()
+
+    """
+    Optional.
+    Since the controller is a node in ISY, it will actual show up as a node.
+    Thus it needs to know the drivers and what id it will use. The controller
+    should report the node server status and have any commands that are
+    needed to control operation of the node server.
+
+    Typically, node servers will use the 'ST' driver to report the node server
+    status and it a best pactice to do this unless you have a very good
+    reason not to.
+
+    The id must match the nodeDef id="controller" in the nodedefs.xml
+    """
+    id = 'controller'
+    commands = {
+        'QUERY': query,
+        'DISCOVER': discover,
+        'REMOVE_NOTICES_ALL': remove_notices_all,
+        
+    }
+    drivers = [
+        {'driver': 'ST', 'value': 1, 'uom': 2},
+    ]
